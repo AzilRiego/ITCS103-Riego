67ca0e80914502d19ff48ca24721fa0d2e80f4c1
Nhevaris Veyne <azilriego@gmail.com>
2025-03-11 10:13:38 +0800

    Add files via upload

--- tkinter_test.py
+++ tkinter_test.py
@@ -5,15 +5,18 @@ window.geometry('300x300')
 window.title("TEST")
 
 #Label
-Label = tk.Label(master = window, text = "Clicking the button does nothing.", font = "Calibary 10 bold")
+Label = tk.Label(master = window, text = "Clicking the button does nothing.", font = "Calibary 30 bold")
 Label.pack()
 
+#Button
 button = tk.Button(master = window, text = "Click")
 button.pack()
 
-Label = tk.Label(master = window, text = "Neither does typigng something.", font = "Calibary 5 bold")
+#Label 2
+Label = tk.Label(master = window, text = "Neither does typigng something.", font = "Calibary 10 bold")
 Label.pack()
 
+#Type
 entry = tk.Entry(master = window)
 entry.pack()
 
